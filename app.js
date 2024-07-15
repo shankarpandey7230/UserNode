b27573c1790765a36d493181b2ef423285cb8658
shankarpandey7230 <shankarpandey7230@gmail.com>
2024-07-15 12:58:21 +1000

    response from html file from server

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 import express from 'express';
+
+import path from 'path';
 const app = express();
 
 const PORT = 8000;
 
-app.get('/', (req, res) => {
-  //   console.log(req, res, next);
+const __dirname = path.resolve();
+console.log(__dirname, '===');
 
-  console.log('we got the request');
-  //   must always return
-  res.send('from server');
+// app.get('/', (req, res) => {
+//   //   console.log(req, res, next);
+
+//   console.log('we got the request');
+//   //   must always return
+//   res.send('<h1>Hello server</h1><hr><p>lorem ipsum lorem ipsum</p>');
+// });
+
+app.get('/', (req, res) => {
+  res.sendFile(
+    __dirname + '/src/html/index.html'
+    // '/Users/shankarpandey/Desktop/DEVELOPER/SHANKAR/Code/BACKEND/User/src/html/index.html'
+  );
 });
 
 app.listen(PORT, (error) => {
